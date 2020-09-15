e58d316d1bcfeb617f2c0a0a4eabcbc2380b4135
liuqingrui <1343923188@qq.com>
2020-09-15 10:42:20 +0800

    fix cookie too long bug

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -56,7 +56,8 @@ const actions = {
         }
         data.businessRole.permissionContent = undefined
         Cookies.set('userInfo', JSON.stringify(data))
-        Cookies.set('permission', JSON.stringify(permission))
+        // Cookies.set('permission', JSON.stringify(permission))
+        sessionStorage.setItem('permission', JSON.stringify(permission))
         resolve()
       }).catch(error => {
         reject(error)
@@ -70,7 +71,8 @@ const actions = {
       console.log('userInfo')
       const userInfo = JSON.parse(Cookies.get('userInfo'))
       console.log(userInfo)
-      const permission = JSON.parse(Cookies.get('permission'))
+      // const permission = JSON.parse(Cookies.get('permission'))
+      const permission = JSON.parse(sessionStorage.getItem('permission'))
       console.log(permission)
       var adminToken = {
         roles: permission,
